aa2174a089016fc3f7e46106359a296f9311665a
landerson1511 <landerson1511@gmail.com>
2021-11-03 16:51:43 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingDisplay = document.querySelector(".remaining span");
+const guessMessage = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const placeHolder = function (word) {
+    const placeHolderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeHolderLetters.push("●");
+    }
+    wordInProgress.innerText = placeHolderLetters.join("");
+};
+
+placeHolder(word);
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});
