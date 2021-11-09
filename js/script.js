4a74eab3018b0e5740ee7f000f13ee082f4578f9
landerson1511 <landerson1511@gmail.com>
2021-11-08 20:31:31 -0600

    added functions to accept & validate player guesses

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const guessMessage = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeHolder = function (word) {
     const placeHolderLetters = [];
@@ -22,7 +23,36 @@ placeHolder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    guessMessage.innerText = "";
     const guess = letterInput.value;
-    console.log(guess);
     letterInput.value = "";
+   const goodGuess = validateInput(guess);
+   if (goodGuess) {
+       makeGuess(guess);
+   }
+   
 });
+
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        guessMessage.innerText = "Please enter a letter";
+    } else if (input.length > 1) {
+        guessMessage.innerText = "Guess only one letter at a time";
+    } else if (!input.match(acceptedLetter)) {
+        guessMessage.innerText = "Please enter a letter from A to Z"
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        guessMessage.innerText = "You already guessed that letter. Try again";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+}
